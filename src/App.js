b63e272761847d4969b5949354308d7a02ed60db
natalikalinina1 <kidmil@mail.ru>
2023-10-04 11:38:25 +0300

    useContext

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import Home from "./pages/Home";
 import {Routes,Route} from 'react-router-dom';
 import NotFound from "./pages/NotFound";
 import Cart from "./pages/Cart";
-
+export const SearchContext = React.createContext("");
 
 function App() {
   const [searchValue, setSearchValue] = React.useState("");
+  
 
   return (
     <div className="wrapper">
-      <Header searchValue={searchValue} setSearchValue={setSearchValue} />
+     <SearchContext.Provider value ={{searchValue, setSearchValue}}>
+      <Header />
       <div className="content">
         <Routes>
           <Route path="/" element={<Home searchValue={searchValue} />} />
@@ -20,6 +22,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
+      </SearchContext.Provider>
     </div>
   );
 }
