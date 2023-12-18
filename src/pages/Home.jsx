bb8b8562d3aac15ccd8b7db02cc3c6a3b29ed602
natalikalinina1 <kidmil@mail.ru>
2023-12-18 11:28:31 +0300

    added pizzaSlice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,6 +12,7 @@ import Categories from "../components/Categories/Categories";
 import Sort from "../components/Sort/Sort";
 import PizzaBlock from "../components/PizzaBlock/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
+import { setItems } from "../redux/pizzaSlice";
 import { SearchContext } from "../App";
 import axios from "axios";
 import {
@@ -27,12 +28,13 @@ const Home = () => {
   const dispatch = useDispatch();
   const isSearch = React.useRef(false);
   const isMounted = React.useRef(false);
+
+  const items = useSelector((state) => state.pizza.items);
   const { categoryId, sort, currentPage } = useSelector(
     (state) => state.filter
   );
 
   const { searchValue } = React.useContext(SearchContext);
-  const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
 
   const onChangeCategory = (id) => {
@@ -44,21 +46,26 @@ const Home = () => {
     dispatch(setCurrentPage(number));
   };
 
-  const FetchPizzas = () => {
+  const FetchPizzas = async () => {
     setIsLoading(true);
     const category = categoryId > 0 ? `&category=${categoryId}` : "";
     const search = searchValue ? `&search=${searchValue}` : "";
     const sortBy = sort.sortProperty.replace("-", "");
     const order = sort.sortProperty.includes("-") ? "asc" : "desc";
-
-    axios
-      .get(
+    try {
+      const { data } = await axios.get(
         `https://63334d6b573c03ab0b5bcff0.mockapi.io/items?&page=${currentPage}&limit=4${category}&sortBy=${sortBy}&order=${order}${search}`
-      )
-      .then((res) => {
-        setItems(res.data);
-        setIsLoading(false);
-      });
+      );
+      dispatch(setItems(data));
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      console.log("ERROR", error);
+      alert("Ошибка при получении пицц");
+    } finally {
+      setIsLoading(false);
+    }
+    window.scrollTo(0, 0);
   };
   // если был 1 рендер  то проверяем параметры URl и сохраняем в reduxe
   React.useEffect(() => {
