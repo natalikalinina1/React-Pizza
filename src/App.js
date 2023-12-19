ab09e48f1b9e66f83e1874109e9e94150fea19e7
natalikalinina1 <kidmil@mail.ru>
2023-12-19 13:45:39 +0300

    added Outlett

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "./scss/app.scss";
-
-import Header from "./components/Header/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
 import FullPizza from "./pages/FullPizza";
+import MainLayout from "./Layouts/MainLayout";
 
 function App() {
-
   return (
-    <div className="wrapper">
-        <Header />
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            <Route path="/pizza/:id" element={<FullPizza />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-    </div>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route path="" element={<Home />} />
+        <Route path="cart" element={<Cart />} />
+        <Route path="pizza/:id" element={<FullPizza />} />
+        <Route path="*" element={<NotFound />} />
+      </Route>
+    </Routes>
   );
 }
 
