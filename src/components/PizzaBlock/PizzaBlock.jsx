f0cf393a07b5b985facb3f5fd3e7214f7a6eaa56
natalikalinina1 <kidmil@mail.ru>
2023-12-19 10:13:29 +0300

    added useParams hook, FullPizza page

--- src/components/PizzaBlock/PizzaBlock.jsx
+++ src/components/PizzaBlock/PizzaBlock.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addItem, selectCartItemById } from "../../redux/cartSlice";
+import { Link } from "react-router-dom";
 
 const typeNames = ["Тонкое", "Традиционное"];
 
@@ -34,8 +35,10 @@ export default function PizzaBlock({
   return (
     <div className="pizza-block-wrapper">
       <div className="pizza-block">
-        <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
-        <h4 className="pizza-block__title">{title}</h4>
+      <Link to={`pizza/${id}`}>
+          <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
+          <h4 className="pizza-block__title">{title}</h4>
+        </Link>
         <div className="pizza-block__selector">
           <ul>
             {types.map((type) => (
